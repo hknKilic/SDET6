6d5c8ee66bc5d968791c5269368b3f49021865ff
TechnoStudy <ismettechno@gmail.com>
2023-08-24 19:50:52 +0100

    Selenium Gun11 Robot FileUpload 2

--- src/Gun12/_04_RobotFileUpload.java
+++ src/Gun12/_04_RobotFileUpload.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.awt.*;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 import java.time.Duration;
 import java.util.List;
@@ -31,21 +32,36 @@ public class _04_RobotFileUpload extends BaseDriver {
             acceptAll.get(0).click();
 
         /*********************/
-//
-//
-//        Robot robot = new Robot();
-//
-//        for(int i=0;i<21;i++){
-//            robot.keyPress(KeyEvent.VK_TAB);
-//            robot.keyRelease(KeyEvent.VK_TAB);
-//        }
-//
-//        robot.keyPress(KeyEvent.VK_ENTER);
-//        robot.keyRelease(KeyEvent.VK_ENTER);
 
 
+        Robot robot = new Robot();
 
-    //   BekleVeKapat();
+        for(int i=0;i<21;i++){
+            robot.keyPress(KeyEvent.VK_TAB);
+            robot.keyRelease(KeyEvent.VK_TAB);
+        }
+
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+
+        // stringi hafızaya kopyalama yöntemi
+        StringSelection dosyaYolu = new StringSelection("C:\\Users\\TechnoStudy\\Desktop\\dosya.txt");
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(dosyaYolu, null);
+
+        MyFunc.Bekle(1);
+        robot.keyPress(KeyEvent.VK_CONTROL); // bu iki tuş şu anda basılmış vaziyette, hafızadaki yapıştırıldı
+        robot.keyPress(KeyEvent.VK_V);
+
+        MyFunc.Bekle(1);
+        robot.keyRelease(KeyEvent.VK_CONTROL); // tuşlar serbest bırakıldı
+        robot.keyRelease(KeyEvent.VK_V);
+
+        MyFunc.Bekle(1);
+        robot.keyPress(KeyEvent.VK_ENTER); // dosya Entera basılarak gönderildi
+        robot.keyRelease(KeyEvent.VK_ENTER);
+
+
+        BekleVeKapat();
     }
 
 
