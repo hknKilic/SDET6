685d509099ea6bbff10f74ce99f99195523a3766
TechnoStudy <ismettechno@gmail.com>
2023-08-28 18:35:34 +0100

    Selenium Gun13 _03_WindowSize fix

--- src/Gun13/_03_WindowSize.java
+++ src/Gun13/_03_WindowSize.java
@@ -14,6 +14,10 @@ public class _03_WindowSize extends BaseDriver {
         MyFunc.Bekle(5);
 
         driver.manage().window().maximize(); // ekranı max yapıyordu.
+        Dimension ekranBoyutu =driver.manage().window().getSize();
+        System.out.println("ekranBoyutu.width = " + ekranBoyutu.width);
+        System.out.println("ekranBoyutu.height = " + ekranBoyutu.height);
+
         MyFunc.Bekle(5);
         Dimension yeniBoyut=new Dimension(516,600);
 
